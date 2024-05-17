31dab74b9871c58444171d0491cdecf30532d272
Kyrylo <134101146+kyrylomatkash@users.noreply.github.com>
2024-05-17 23:59:29 +0200

    Update Faq.jsx

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
 
 const FAQs = () => {
   return (
     <Box>
-      <Heading as="h3" size="lg" mb={4}>
+      <Heading as="h3" size="lg" mb={6}>
         Frequently Asked Questions
       </Heading>
-      <Box>
-        <Box mb={4}>
-          <Heading as="h4" size="md">
-            Question 1?
-          </Heading>
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
-        </Box>
-        <Box mb={4}>
-          <Heading as="h4" size="md">
-            Question 2?
-          </Heading>
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
-        </Box>
-      </Box>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                <Heading as="h4" size="md">
+                  Question 1?
+                </Heading>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                <Heading as="h4" size="md">
+                  Question 2?
+                </Heading>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Text>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Box>
   );
 };
